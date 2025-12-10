63c3d10588cacee392b66571bd5b8a82cdc09b26
José Segovia <acosta.segoviaa@hotmail.com>
2025-12-10 00:25:14 -0500

    fix: vite build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './test/setup.ts',
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './src/main.tsx',
-      },
-    },
-  },
+  base: '/',
 });
